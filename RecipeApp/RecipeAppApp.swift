3b09ab7a9706cccf254dd8fb50fe8d99607865d3
Abdul Rahim <abdul7716@gmail.com>
2024-10-06 10:14:04 -0500

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Abdul Choudhary on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
